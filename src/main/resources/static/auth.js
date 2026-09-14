da10af1cd736824a1fec400ef02cc64e5ef6b14d
André Pereira <amanuelmpereira@gmail.com>
2026-09-14 15:40:43 +0100

    Atualização completa da aplicação

---
+++ src/main/resources/static/auth.js
@@ -0,0 +1,27 @@
+// Base de dados simulada de utilizadores e permissões (Gerido via JS / JSON)
+const utilizadoresPermissoes = [
+    {
+        utilizador: "JoaoOliveira",
+        palavraPasse: "Treinador1",
+        nomeCompleto: "João Oliveira",
+        role: "coordenador", // Acede a todos os escalões e gestão total
+        escaloesPermitidos: ["todos"] 
+    },
+    {
+        utilizador: "AndrePereira",
+        palavraPasse: "Andre1",
+        nomeCompleto: "André Pereira",
+        role: "Gestor Aplicação", // Acede apenas ao escalão definido
+        escaloesPermitidos: ["todos"] 
+    },
+    {
+        utilizador: "TreinadorBenjamins",
+        palavraPasse: "Benjamins123",
+        nomeCompleto: "Carlos Silva",
+        role: "treinador",
+        escaloesPermitidos: ["Benjamins"] 
+    }
+];
+
+// Variável global para guardar a sessão do utilizador atualmente autenticado
+let utilizadorLogado = null;
